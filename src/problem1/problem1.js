2ce23ebb6e256992b18ea3521f3a82d5c9c34733
anh.nt <anh.nt@vn-cubesystem.com>
2024-06-01 20:30:55 +0700

    feat: Problem1

---
+++ src/problem1/problem1.js
@@ -0,0 +1,25 @@
+var sum_to_n_a = function(n) {
+  // Using loop
+  let res = 0;
+
+  for (let i = 0; i <= n; i++){
+    res += i;
+  }
+
+  return res;
+};
+
+var sum_to_n_b = function(n) {
+  // Using recursion
+  if(n <= 1){
+    return n;
+  }
+
+  return n + sum_to_n_b(n - 1);
+};
+
+var sum_to_n_c = function(n) {
+  // Using arithmetic progression
+
+  return ((n + 1) / 2)*n
+};
